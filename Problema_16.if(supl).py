1196d31f137dc7014906aac1b018a8153ce7287f
BabalauCristian10C <70657828+BabalauCristian10C@users.noreply.github.com>
2020-10-19 03:50:17 +0800

    Update Problema_16.if(supl).py

--- Problema_16.if(supl).py
+++ Problema_16.if(supl).py
@@ -1,4 +1,3 @@
-#calcularea ecuatiilor de gradul2 
 import math
 
 print("Forma ecuatiei de gr2 : ax**2 + bx + c")
@@ -10,23 +9,22 @@ x = "x"
 
 def rez(a,b,c):
     disc = b**2 - 4*a*c
-    if a != 0:
-        if b != 0:
-            if disc < 0:
-                print(f'Discriminantul este negativ, ecuatia nu are solutii {disc}')
-            elif disc == 0:
-                sol_0 = (b**2) / (2 * a)
-                print("Unica solutie este", sol_0)
-            else:
-                sol_1 = int((-1 * b - math.sqrt(disc)) / 2 * a)
-                sol_2 = int((-1 * b + math.sqrt(disc)) / 2 * a)
-                print(f'Solutiile sunt {sol_1} si {sol_2}')
+    if a != 0 and b != 0 and c != 0:
+        if disc < 0:
+            print(f'Discriminantul este negativ, ecuatia nu are solutii {disc}')
+        elif disc == 0:
+            sol_0 = (b**2) / (2 * a)
+            print("Unica solutie este", sol_0)
+        else:
+            sol_1 = int((-1 * b - math.sqrt(disc)) / 2 * a)
+            sol_2 = int((-1 * b + math.sqrt(disc)) / 2 * a)
+            print(f'Solutiile sunt {sol_1} si {sol_2}')
     elif (a == 0) and (c != 0) and (b != 0):
         sol_3 = float(-c/b)
         print(sol_3)
     elif (a != 0) and (b == 0) and( c != 0):
-        sol_4 = float(math.sqrt(c)/a)
+        sol_4 = float(math.sqrt(c))
         sol_5 = -sol_4
-        print(sol_5, sol_4)
+        return sol_5 , sol_4
 
 print(rez(a, b, c))
